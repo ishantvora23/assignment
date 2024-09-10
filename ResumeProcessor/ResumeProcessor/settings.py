261d766ac49e5a76edf6e6b7434e29d44e27b228
ishantvora23 <157264664+ishantvora23@users.noreply.github.com>
2024-09-10 17:39:07 +0530

    Update settings.py

--- ResumeProcessor/ResumeProcessor/settings.py
+++ ResumeProcessor/ResumeProcessor/settings.py
@@ -75,13 +75,26 @@ WSGI_APPLICATION = 'ResumeProcessor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+#DATABASES = {
+    #'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': BASE_DIR / 'db.sqlite3',
+ #   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'resume',
+        'USER': 'postgres',
+        'PASSWORD': 'deep21609',
+        'HOST': 'localhost',  # Set to your PostgreSQL server address
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
